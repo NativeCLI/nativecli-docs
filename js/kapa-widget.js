56a7291e082d51c2ddeef0da6a86deec59f8db47
Pete Bishop <9081809+PeteBishwhip@users.noreply.github.com>
2025-11-07 09:07:05 +0000

    Add Kapa.ai chat to docs

---
+++ js/kapa-widget.js
@@ -0,0 +1,9 @@
+const script = document.createElement("script");
+script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+script.async = true;
+script.setAttribute("data-website-id", "43432967-3098-4dc3-8ecd-0e5a69a7005f");
+script.setAttribute("data-project-name", "NativeCLI");
+script.setAttribute("data-project-color", "#07C983");
+script.setAttribute("data-project-logo", "/images/native-php.png");
+script.setAttribute("data-consent-required", "true");
+document.head.appendChild(script);
